0e72dffdf9ae5501d7ab50c3379f813d0d9cee32
Dagger3 <66926121+Dagger3@users.noreply.github.com>
2020-06-18 14:39:09 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 
+
+
 // This function prints out your name and favorite color
 function nameAndColor(name:string, color:string){
 	console.log("Hello " + name + ". Your favorite color is " + color)
@@ -14,3 +16,59 @@ function nameAndNumber(name:string, favNum:number){
 }
 
 nameAndNumber("Ryan", 3)
+
+// make a new function that prints the users name and grade
+function nameAndGrade(name:string, g:number){
+    console.log("Hello " + name + ". Your grade number is " + g)
+}
+
+nameAndGrade("Ryan", 8)
+
+//make a new function that prints out the users name and school
+function nameAndSchool(name:string, s:string){
+    console.log("Hello " + name + ". My school is " + s)
+}
+
+nameAndSchool("Ryan", "Miller Middle School")
+
+//make a new function that prints out the users name, grade, and school
+function nameAndGradeAndSchool(name:string, g:number, s:string){
+    console.log("Hello " + name + ". Your grade number is " + g + ". My school is " + s)
+}
+
+nameAndGradeAndSchool("Ryan", 8, "Miller Middle School")
+
+
+function multiplyByTwo(startingNum:number){
+    let result = startingNum *2
+    console.log(result)
+}
+
+multiplyByTwo(5)
+
+
+
+function multiplyByFive(startingNum:number){
+    let result = startingNum * 5
+    console.log(result)
+}
+
+multiplyByFive(5)
+
+
+//make a new function that divides a number by 2
+function divideByTwo(startingNum:number){
+    let result = startingNum / 2
+    console.log(result)
+}
+
+divideByTwo(4)
+
+
+//make a function that divides a number by 2 then adds 20
+function divideByTwoAndAddTwenty(startingNum:number){
+    let result = startingNum / 2 +20
+    console.log(result)
+}
+
+divideByTwoAndAddTwenty(4)
