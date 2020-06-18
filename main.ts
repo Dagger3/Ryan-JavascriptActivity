385de9d607088f2174a3f21878754d733c6bb093
Dagger3 <66926121+Dagger3@users.noreply.github.com>
2020-06-18 13:28:23 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+
+// This function prints out your name and favorite color
+function nameAndColor(name:string, color:string){
+	console.log("Hello " + name + ". Your favorite color is " + color)
+}
+
+
+nameAndColor("Ryan", "Purple")
+
+
+// This function prints out your name and favorite number
+function nameAndNumber(name:string, favNum:number){
+    console.log("Hello " + name + ". Your favorite number is " + favNum)
+}
+
+nameAndNumber("Ryan", 3)
